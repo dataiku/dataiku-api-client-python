1ccff1d7b7416d4bd358bfc16a410c9bf2f60c42
Frederic Chataigner <frederic.chataigner@dataiku.com>
2015-11-26 12:47:09 +0100

    add extra conf for hive queries

--- dataikuapi/dssclient.py
+++ dataikuapi/dssclient.py
@@ -82,7 +82,7 @@ class DSSClient(object):
     # SQL queries
     ########################################################
 
-    def sql_query(self, query, connection=None, database=None, dataset_full_name=None, pre_queries=None, post_queries=None, type='sql'):
+    def sql_query(self, query, connection=None, database=None, dataset_full_name=None, pre_queries=None, post_queries=None, type='sql', extra_conf={}):
         """
         Initiate a SQL, Hive or Impala query and get a handle to retrieve the results of the query.
         Internally, the query is run by DSS. The  database to run the query on is specified either by 
@@ -101,7 +101,7 @@ class DSSClient(object):
         Returns:
             A :class:`dataikuapi.dss.sqlquery.DSSSQLQuery` query handle
         """
-        return DSSSQLQuery(self, query, connection, database, dataset_full_name, pre_queries, post_queries, type)
+        return DSSSQLQuery(self, query, connection, database, dataset_full_name, pre_queries, post_queries, type, extra_conf)
 
     ########################################################
     # Users
