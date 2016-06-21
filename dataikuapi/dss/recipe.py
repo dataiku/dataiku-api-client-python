efff6a05a21c82d4e84f0fd7e5941cab0190ea9a
Frederic Chataigner <frederic.chataigner@dataiku.com>
2016-06-21 15:40:33 +0200

    more wrapping of recipe fields

--- dataikuapi/dss/recipe.py
+++ dataikuapi/dss/recipe.py
@@ -27,27 +27,28 @@ class DSSRecipe(object):
     # Recipe definition
     ########################################################
     
-    def get_definition(self):
+    def get_definition_and_payload(self):
         """
         Get the definition of the recipe
         
         Returns:
-            the definition, as a JSON object, containing the recipe definition itself and its payload
+            the definition, as a DSSRecipeDefinitionAndPayload object, containing the recipe definition itself and its payload
         """
-        return self.client._perform_json(
+        data = self.client._perform_json(
                 "GET", "/projects/%s/recipes/%s/" % (self.project_key, self.recipe_name))
+        return DSSRecipeDefinitionAndPayload(data)
 
-    def set_definition(self, definition):
+    def set_definition_and_payload(self, definition):
         """
         Set the definition of the recipe
         
         Args:
-            definition: the definition, as a JSON object. You should only set a definition object 
+            definition: the definition, as a DSSRecipeDefinitionAndPayload object. You should only set a definition object 
             that has been retrieved using the get_definition call.
         """
         return self.client._perform_json(
                 "PUT", "/projects/%s/recipes/%s/" % (self.project_key, self.recipe_name),
-                body=definition)
+                body=definition.data)
 
     ########################################################
     # Recipe metadata
@@ -78,4 +79,36 @@ class DSSRecipe(object):
                 body=metadata)
 
 
-             
+class DSSRecipeDefinitionAndPayload(object):
+    """
+    Definition for a recipe, that is, the recipe definition itself and its payload
+    """
+    def __init__(self, data):
+        self.data = data
+        
+    def get_recipe_raw_definition(self):
+        return self.data.get('recipe', None)
+        
+    def get_recipe_inputs(self):
+        return self.data.get('recipe').get('inputs')
+
+    def get_recipe_outputs(self):
+        return self.data.get('recipe').get('outputs')
+
+    def get_recipe_params(self):
+        return self.data.get('recipe').get('params')
+        
+    def get_payload(self):
+        return self.data.get('payload', None)
+        
+    def get_json_payload(self):
+        return json.loads(self.data.get('payload', None))
+        
+    def set_payload(self, payload):
+        self.data['payload'] = payload
+        
+    def set_json_payload(self, payload):
+        self.data['payload'] = json.dumps(payload)
+        
+    
+    
