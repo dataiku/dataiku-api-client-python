8c7417e2df560dc2d1f7c9f3e036db1c8e4e1ba9
frederic <frederic.chataigner@dataiku.com>
2016-11-09 11:19:44 +0100

    add helpers to create recipes

--- dataikuapi/__init__.py
+++ dataikuapi/__init__.py
@@ -2,3 +2,5 @@ from dssclient import DSSClient
 
 from apinode_client import APINodeClient
 from apinode_admin_client import APINodeAdminClient
+
+from dss.recipe import GroupingRecipeCreator, JoinRecipeCreator, StackRecipeCreator, WindowRecipeCreator, SyncRecipeCreator, SamplingRecipeCreator, ShakerRecipeCreator
--- dataikuapi/dss/recipe.py
+++ dataikuapi/dss/recipe.py
@@ -111,4 +111,135 @@ class DSSRecipeDefinitionAndPayload(object):
         self.data['payload'] = json.dumps(payload)
         
     
+class DSSRecipeCreator(object):
+    """
+    Helper to create new recipes
+    """
+    def __init__(self, type, name, project):
+        self.type = type
+        self.name = name
+        self.project = project
+        self.recipe_inputs = []
+        self.recipe_outputs = []
+        self.recipe_params = {}
+        
+    def _build_ref(self, object_id, project_key=None):
+        if project_key is not None and project_key != self.project.project_key:
+            return project_key + '.' + object_id
+        else:
+            return object_id
     
+    def with_input(self, dataset_name, project_key=None):
+        self.recipe_inputs.append({'ref':self._build_ref(dataset_name, project_key)})
+        return self
+        
+    def with_output(self, dataset_name, append=False):
+        self.recipe_outputs.append({'ref':self._build_ref(dataset_name), 'appendMode':append})
+        return self
+
+    def _get_recipe_inputs(self):
+        return self.recipe_inputs
+        
+    def _get_recipe_outputs(self):
+        return self.recipe_outputs
+        
+    def _get_recipe_params(self):
+        return self.recipe_params
+        
+    def build(self):
+        """
+        Create a new recipe in the project, and return a handle to interact with it. 
+        
+        Returns:
+            A :class:`dataikuapi.dss.recipe.DSSRecipe` recipe handle
+        """
+        effective_inputs = self._get_recipe_inputs()
+        effective_outputs = self._get_recipe_outputs()
+        effective_params = self._get_recipe_params()
+        return self.project.create_recipe(self.name, self.type, effective_inputs, effective_outputs, effective_params)
+
+
+class SingleOutputRecipeCreator(DSSRecipeCreator):
+    def __init__(self, type, name, project):
+        DSSRecipeCreator.__init__(self, type, name, project)
+        self.create_output_dataset = None
+        self.output_dataset_settings = None
+
+    def with_existing_output(self, dataset_name, append=False):
+        assert self.create_output_dataset is None
+        self.create_output_dataset = False
+        self.recipe_outputs.append({'ref':self._build_ref(dataset_name), 'appendMode':append})
+        return self
+        
+    def with_new_output(self, dataset_name, connection_id, format_option_id=None, partitioning_option_id=None, append=False):
+        assert self.create_output_dataset is None
+        self.create_output_dataset = True
+        self.output_dataset_settings = {'connectionId':connection_id,'formatOptionId':format_option_id,'partitioningOptionId':partitioning_option_id}
+        self.recipe_outputs.append({'ref':self._build_ref(dataset_name), 'appendMode':append})
+        return self
+
+    def with_output(self, dataset_name, append=False):
+        return self.with_existing_output(dataset_name, append)
+        
+    def _get_recipe_params(self):
+        return {'createOutputDataset':self.create_output_dataset, 'outputDatasetSettings':self.output_dataset_settings}
+
+class VirtualInputsSingleOutputRecipeCreator(SingleOutputRecipeCreator):
+    def __init__(self, type, name, project):
+        SingleOutputRecipeCreator.__init__(self, type, name, project)
+        self.virtual_inputs = []
+
+    def with_input(self, dataset_name, project_key=None):
+        self.virtual_inputs.append(self._build_ref(dataset_name, project_key))
+        return self
+        
+    def _get_recipe_params(self):
+        params = super(VirtualInputsSingleOutputRecipeCreator, self)._get_recipe_params()
+        params['virtualInputs'] = self.virtual_inputs
+        return params
+
+########################
+#
+# actual recipe creators
+#
+########################
+class WindowRecipeCreator(SingleOutputRecipeCreator):
+    def __init__(self, name, project):
+        SingleOutputRecipeCreator.__init__(self, 'window', name, project)
+        
+class SyncRecipeCreator(SingleOutputRecipeCreator):
+    def __init__(self, name, project):
+        SingleOutputRecipeCreator.__init__(self, 'sync', name, project)
+        
+class GroupingRecipeCreator(SingleOutputRecipeCreator):
+    def __init__(self, name, project):
+        SingleOutputRecipeCreator.__init__(self, 'grouping', name, project)
+        self.group_key = None
+
+    def with_group_key(self, group_key):
+        self.group_key = group_key
+        return self
+        
+    def _get_recipe_params(self):
+        params = super(GroupingRecipeCreator, self)._get_recipe_params()
+        params['groupKey'] = self.group_key
+        return params
+
+class JoinRecipeCreator(VirtualInputsSingleOutputRecipeCreator):
+    def __init__(self, name, project):
+        VirtualInputsSingleOutputRecipeCreator.__init__(self, 'join', name, project)
+
+class StackRecipeCreator(VirtualInputsSingleOutputRecipeCreator):
+    def __init__(self, name, project):
+        VirtualInputsSingleOutputRecipeCreator.__init__(self, 'vstack', name, project)
+
+class SamplingRecipeCreator(SingleOutputRecipeCreator):
+    def __init__(self, name, project):
+        SingleOutputRecipeCreator.__init__(self, 'sampling', name, project)
+ 
+# shaker needs the unsafe code permission which is not on api keys       
+#class ShakerRecipeCreator(SingleOutputRecipeCreator):
+#    def __init__(self, name, project):
+#        SingleOutputRecipeCreator.__init__(self, 'shaker', name, project)
+        
+        
