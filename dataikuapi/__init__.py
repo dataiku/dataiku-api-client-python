6a3dbec0935cdd60a320072f35e3755c73c5f6bc
frederic <frederic.chataigner@dataiku.com>
2017-01-05 12:05:09 +0100

    forgotten import

--- dataikuapi/__init__.py
+++ dataikuapi/__init__.py
@@ -3,4 +3,4 @@ from dssclient import DSSClient
 from apinode_client import APINodeClient
 from apinode_admin_client import APINodeAdminClient
 
-from dss.recipe import GroupingRecipeCreator, JoinRecipeCreator, StackRecipeCreator, WindowRecipeCreator, SyncRecipeCreator, SamplingRecipeCreator, SQLQueryRecipeCreator #, ShakerRecipeCreator
+from dss.recipe import GroupingRecipeCreator, JoinRecipeCreator, StackRecipeCreator, WindowRecipeCreator, SyncRecipeCreator, SamplingRecipeCreator, SQLQueryRecipeCreator, CodeRecipeCreator #, ShakerRecipeCreator
