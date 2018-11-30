f0e81c9e734703e193b7ab073d4803a873403613
Thomas Labadie <thomas.labadie@gmail.com>
2018-11-30 10:57:24 +0100

    Coders XP Python API

--- dataikuapi/dss/plugin.py
+++ dataikuapi/dss/plugin.py
@@ -79,4 +79,62 @@ class DSSPlugin(object):
         data = f.read() # eat it all, because making it work with a path variable and a MultifilePart in swing looks complicated
         return self.client._perform_empty("POST", "/plugins/%s/contents/%s" % (self.plugin_id, path), raw_body=data)
 
+    def delete_file(self, path):
+        """
+        Get a file from the plugin folder
+        
+        Args:
+            path: the name of the file, from the root of the plugin
+
+        Returns:
+            the file's content, as a stream
+        """
+        return self.client._perform_raw("DELETE", "/plugins/%s/contents/%s" % (self.plugin_id, path)).raw
+
+    ########################################################
+    # Dev plugin Git integration
+    ########################################################
+
+    def pull_rebase(self):
+        """
+        Pull the latest version from the current branch of the plugin. Aborts if merge fails.
+        """
+        return self.client._perform_json("GET", "/plugins/%s/actions/pullRebase" % (self.plugin_id))
+
+    def push(self):
+        """
+        Push from the current branch of the plugin.
+        """
+        return self.client._perform_json("GET", "/plugins/%s/actions/push" % (self.plugin_id))
+
+    def fetch(self):
+        """
+        Fetch new content from remote repository.
+        """
+        return self.client._perform_json("GET", "/plugins/%s/actions/fetch" % (self.plugin_id))
+
+    def reset_to_local_head_state(self):
+        """
+        Drop uncommitted changes and resets the current branch to local HEAD.
+        """
+        return self.client._perform_json("GET", "/plugins/%s/actions/resetToLocalHeadState" % (self.plugin_id))
+
+    def reset_to_remote_head_state(self):
+        """
+        Delete all of your non-pushed work on the current branch and resets it to the remote state.
+        """
+        return self.client._perform_json("GET", "/plugins/%s/actions/resetToRemoteHeadState" % (self.plugin_id))
+
+    def set_remote(self, repository_URL):
+        """
+        Sets the URL of the Git remote origin for your local repository.
+        """
+        return self.client._perform_json("POST", "/plugins/%s/gitRemote" % (self.plugin_id), body={'repositoryUrl': repository_URL})
+
+    def delete_remote(self, repository_URL):
+        """
+        Removes the URL of the Git remote origin for your local repository.
+        """
+        return self.client._perform_json("DELETE", "/plugins/%s/gitRemote" % (self.plugin_id))
+
         
