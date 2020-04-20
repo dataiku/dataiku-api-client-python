154fc995fa829f503467efa1b92d5fe2083d2338
Clement Stenac <clement.stenac@dataiku.com>
2020-04-20 14:39:37 +0200

    Consistency fixes

--- dataikuapi/dss/flow.py
+++ dataikuapi/dss/flow.py
@@ -129,13 +129,14 @@ class DSSProjectFlowGraph(object):
         """
         return [self._get_object_from_graph_node(x) for x in self.get_source_computables() if x["type"] == "COMPUTABLE_DATASET"]
 
-    def get_successor_recipes(self, node, as_type="name"):
+    def get_successor_recipes(self, node, as_type="dict"):
         """
         Returns a list of recipes that are a successor of a graph node
 
         :param node: Either a name or :class:`dataikuapi.dss.dataset.DSSDataset` object
-        :return if as_type="name", list of strings, recipe names
-                    else list of :class:`dataikuapi.dss.recipe.DSSRecipe`
+        :param as_type: How to return the successor recipes. Possible values are "dict" and "object"
+        :return if as_type=dict, each recipes is returned as a dict containing at least "ref" and "type". 
+                if as_type=object, each computable is returned as a  :class:`dataikuapi.dss.recipe.DSSRecipe`,
         """
         if isinstance(node, DSSDataset):
             node = node.dataset_name
@@ -150,7 +151,10 @@ class DSSProjectFlowGraph(object):
     def get_successor_computables(self, node, as_type="dict"):
         """
         Returns a list of computables that are a successor of a given graph node
-        Each computable is returned as a dict containing at least "ref" and "type"
+        
+        :param as_type: How to return the successor recipes. Possible values are "dict" and "object"
+        :return if as_type=dict, each recipes is returned as a dict containing at least "ref" and "type". 
+                if as_type=object, each computable is returned as a  :class:`dataikuapi.dss.recipe.DSSRecipe`,
         """
         if isinstance(node, DSSRecipe):
             node = node.recipe_name
