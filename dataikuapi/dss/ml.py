71a71e9774fcc33b2b2f88451ec915e9c1453cb1
Frederic Chataigner <frederic.chataigner@dataiku.com>
2021-04-15 16:25:01 +0200

    add method to check whether there is a time ordering

--- dataikuapi/dss/ml.py
+++ dataikuapi/dss/ml.py
@@ -170,12 +170,19 @@ class PredictionSplitParamsHandler(object):
 
         return self
 
+    def has_time_ordering(self):
+        """
+        :return: whether the splitting uses time ordering
+        :rtype: bool
+        """
+        return 'time' in self.mltask_settings and self.mltask_settings['time']['enabled']
+
     def get_time_ordering_variable(self):
         """
         :return: the name of the variable
         :rtype: str
         """
-        if 'time' in self.mltask_settings and self.mltask_settings['time']['enabled']:
+        if self.has_time_ordering():
             return self.mltask_settings['time']['timeVariable']
         else:
             warnings.warn("Time-based ordering is disabled in the current MLTask")
@@ -185,7 +192,7 @@ class PredictionSplitParamsHandler(object):
         :return: True if the ordering is set to be ascending with respect to the time-ordering variable
         :rtype: bool
         """
-        if 'time' in self.mltask_settings and self.mltask_settings['time']['enabled']:
+        if self.has_time_ordering():
             return self.mltask_settings['time']['ascending']
         else:
             warnings.warn("Time-based ordering is disabled in the current MLTask")
