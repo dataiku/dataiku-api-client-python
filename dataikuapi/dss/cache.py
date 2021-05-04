684eeda32317abe31f9b32de5deab0ca3bcab5ca
avigny <alexandre.vigny@dataiku.com>
2021-05-04 18:32:07 +0200

    quoting cache entry keys

--- dataikuapi/dss/cache.py
+++ dataikuapi/dss/cache.py
@@ -1,3 +1,6 @@
+from dataikuapi.utils import dku_quote
+
+
 class DSSCache(object):
     def __init__(self, client, project):
         self.client = client
@@ -8,13 +11,16 @@ class DSSCache(object):
         Gets the cache entry if it exists
         Returns the value or raises an exception if entry does not exists
         """
-        resp = self.client._perform_raw("GET", "/projects/%s/cache/%s" % (self.project.project_key, key))
+        resp = self.client._perform_raw("GET", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)))
         if resp.status_code == 200:
             # cache hit
-            return resp.json()
+            return {
+                "cache_hit": True,
+                "value": resp.json(),
+            }
         elif resp.status_code == 204:
             # cache miss
-            return None
+            return {"cache_hit": False}
         raise Exception("WTF")
 
     def set_entry(self, key, value, ttl=0):
@@ -22,14 +28,14 @@ class DSSCache(object):
         Sets the cache entry with an optional ttl (defaults to infinite)
         """
         if ttl > 0:
-            self.client._perform_raw("POST", "/projects/%s/cache/%s?ttl=%s" % (self.project.project_key, key, ttl),
+            self.client._perform_raw("POST", "/projects/%s/cache/%s?ttl=%s" % (self.project.project_key, dku_quote(key), ttl),
                                       body={value})
         else:
-            self.client._perform_raw("POST", "/projects/%s/cache/%s" % (self.project.project_key, key),
+            self.client._perform_raw("POST", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)),
                                       body={value})
 
     def delete_entry(self, key):
         """
         Deletes the entry
         """
-        self.client._perform_raw("DELETE", "/projects/%s/cache/%s" % (self.project.project_key, key))
+        self.client._perform_raw("DELETE", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)))
