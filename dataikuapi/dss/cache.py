7bba0689e26912434d72759e5c569365d42d9cfb
avigny <alexandre.vigny@dataiku.com>
2021-05-05 18:53:03 +0200

    day_2

--- dataikuapi/dss/cache.py
+++ dataikuapi/dss/cache.py
@@ -2,16 +2,16 @@ from dataikuapi.utils import dku_quote
 
 
 class DSSCache(object):
-    def __init__(self, client, project):
+    def __init__(self, client, project_key):
         self.client = client
-        self.project = project
+        self.project_key = project_key
 
     def get_entry(self, key):
         """
         Gets the cache entry if it exists
         Returns the value or raises an exception if entry does not exists
         """
-        resp = self.client._perform_raw("GET", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)))
+        resp = self.client._perform_raw("GET", "/projects/%s/cache/%s" % (self.project_key, dku_quote(key)))
         if resp.status_code == 200:
             # cache hit
             return {
@@ -23,32 +23,35 @@ class DSSCache(object):
             return {"cache_hit": False}
         raise Exception("WTF")
 
+    def get_batch(self, keys):
+        resp = self.client._perform_json("POST", "/projects/%s/cache/get_batch")
+
     def set_entry(self, key, value, ttl=0):
         """
         Sets the cache entry with an optional ttl (defaults to infinite)
         """
         if ttl > 0:
-            self.client._perform_raw("POST", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)),
-                                     params={"ttl": ttl}, body={value})
+            self.client._perform_raw("POST", "/projects/%s/cache/%s" % (self.project_key, dku_quote(key)),
+                                     params={"ttl": ttl}, body=value)
         else:
-            self.client._perform_raw("POST", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)),
-                                      body={value})
+            self.client._perform_raw("POST", "/projects/%s/cache/%s" % (self.project_key, dku_quote(key)),
+                                      body=value)
 
     def delete_entry(self, key):
         """
         Deletes the entry
         """
-        self.client._perform_raw("DELETE", "/projects/%s/cache/%s" % (self.project.project_key, dku_quote(key)))
+        self.client._perform_raw("DELETE", "/projects/%s/cache/%s" % (self.project_key, dku_quote(key)))
 
     def delete_cache(self, key):
         """
         Deletes the entry
         """
-        self.client._perform_raw("DELETE", "/projects/%s/cache" % self.project.project_key)
+        self.client._perform_raw("DELETE", "/projects/%s/cache" % self.project_key)
 
     def get_keys(self):
         """
         Gets all entry keys
         Returns a list of entry keys
         """
-        return self.client._perform_json("GET", "/projects/%s/cache" % self.project.project_key)
+        return self.client._perform_json("GET", "/projects/%s/cache" % self.project_key)
