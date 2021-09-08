00f698c377b4c503459c7236115439f8c9456556
Thibaud Baas <thibaud.baas@dataiku.com>
2021-09-08 14:57:18 +0200

    FM: Split Instance and InstanceCreator per cloud

--- dataikuapi/fm/instances.py
+++ dataikuapi/fm/instances.py
@@ -6,9 +6,10 @@ class FMInstanceCreator(object):
         """
         Helper to create a DSS Instance
 
+        :param object client: :class:`dataikuapi.fm.fmclient`
+        :param str label: The label of the instance
         :param str instance_settings_template: The instance settings template id this instance should be based on
         :param str virtual_network: The virtual network where the instance should be spawned
-        :param str label: The label of the instance
         :param str image_id: The ID of the DSS runtime image (ex: dss-9.0.3-default)
         """
         self.client = client
@@ -21,33 +22,29 @@ class FMInstanceCreator(object):
         # Set the default value for dssNodeType
         self.data["dssNodeType"] = "design"
 
-    def create(self):
-        """
-        Create the DSS instance
-
-        :return: Instance
-        :rtype: :class:`dataikuapi.fm.instances.FMInstance`
-        """
-        instance = self.client._perform_tenant_json("POST", "/instances", body=self.data)
-        return FMInstance(self.client, instance)
-
-    def set_dss_node_type(self, dss_node_type):
+    def with_dss_node_type(self, dss_node_type):
         """
         Set the DSS Node type of the instance to create
 
         :param str dss_node_type: Optional , the type of the dss node to create. Supports "design", "automation ordeployer". Defaults to "design"
+        :rtype: :class:`dataikuapi.fm.instances.FMInstanceCreator`
         """
         if dss_node_type not in ["design", "automation", "deployer"]:
             raise ValueError("Only \"design\", \"automation\" or \"deployer\" dss_node_type are supported")
         self.data["dssNodeType"] = dss_node_type
+        return self
 
-    def set_cloud_instance_type(self, cloud_instance_type):
+    def with_cloud_instance_type(self, cloud_instance_type):
         """
         Set the machine type for the DSS Instance
+
+        :param str cloud_instance_type
+        :rtype: :class:`dataikuapi.fm.instances.FMInstanceCreator`
         """
         self.data["cloudInstanceType"] = cloud_instance_type
+        return self
 
-    def set_data_volume_options(self, data_volume_type=None, data_volume_size=None, data_volume_size_max=None, data_volume_IOPS=None, data_volume_encryption=None, data_volume_encryption_key=None):
+    def with_data_volume_options(self, data_volume_type=None, data_volume_size=None, data_volume_size_max=None, data_volume_IOPS=None, data_volume_encryption=None, data_volume_encryption_key=None):
         """
         Set the options of the data volume to use with the DSS Instance
 
@@ -57,6 +54,7 @@ class FMInstanceCreator(object):
         :param int data_volume_IOPS: Optional, Data volume IOPS
         :param object data_volume_encryption: Optional, a :class:`dataikuapi.fm.instances.FMInstanceEncryptionMode` setting the encryption mode of the data volume
         :param str data_volume_encryption_key: Optional, the encryption key to use when data_volume_encryption_key is FMInstanceEncryptionMode.CUSTOM
+        :rtype: :class:`dataikuapi.fm.instances.FMInstanceCreator`
         """
         if type(data_volume_encryption) is not FMInstanceEncryptionMode:
             raise TypeError("data_volume encryption needs to be of type FMInstanceEncryptionMode")
@@ -67,42 +65,71 @@ class FMInstanceCreator(object):
         self.data["dataVolumeIOPS"] = data_volume_IOPS
         self.data["dataVolumeEncryption"] = data_volume_encryption.value
         self.data["dataVolumeEncryptionKey"] = data_volume_encryption_key
+        return self
 
-    def set_aws_root_volume_options(self, aws_root_volume_size=None, aws_root_volume_type=None, aws_root_volume_IOPS=None):
-        """
-        AWS Only: Set the options of the root volume of the DSS Instance
-
-        :param int aws_root_volume_size: Optional, the root volume size
-        :param str aws_root_volume_type: Optional, the root volume type
-        :param int aws_root_volume_IOPS: Optional, the root volume IOPS
-        """
-        if self.client.cloud != "AWS":
-            raise BaseException("set_aws_root_volume_options is only usable on AWS tenants")
-        self.data["awsRootVolumeSizeGB"] = aws_root_volume_size
-        self.data["awsRootVolumeType"] = aws_root_volume_type
-        self.data["awsRootVolumeIOPS"] = aws_root_volume_IOPS
-
-    def set_cloud_tags(self, cloud_tags):
+    def with_cloud_tags(self, cloud_tags):
         """
         Set the tags to be applied to the cloud resources created for this DSS instance
 
         :param dict cloud_tags: a key value dictionary of tags to be applied on the cloud resources
+        :rtype: :class:`dataikuapi.fm.instances.FMInstanceCreator`
         """
         self.data["cloudTags"] = cloud_tags
+        return self
 
-    def set_fm_tags(self, fm_tags):
+    def with_fm_tags(self, fm_tags):
         """
         A list of tags to add on the DSS Instance in Fleet Manager
 
         :param list fm_tags: Optional, list of tags to be applied on the instance in the Fleet Manager
+        :rtype: :class:`dataikuapi.fm.instances.FMInstanceCreator`
         """
         self.data["fmTags"] = fm_tags
+        return self
+
+
+class FMAWSInstanceCreator(FMInstanceCreator):
+    def with_aws_root_volume_options(self, aws_root_volume_size=None, aws_root_volume_type=None, aws_root_volume_IOPS=None):
+        """
+        Set the options of the root volume of the DSS Instance
+
+        :param int aws_root_volume_size: Optional, the root volume size
+        :param str aws_root_volume_type: Optional, the root volume type
+        :param int aws_root_volume_IOPS: Optional, the root volume IOPS
+        :rtype: :class:`dataikuapi.fm.instances.FMAWSInstanceCreator`
+        """
+        self.data["awsRootVolumeSizeGB"] = aws_root_volume_size
+        self.data["awsRootVolumeType"] = aws_root_volume_type
+        self.data["awsRootVolumeIOPS"] = aws_root_volume_IOPS
+        return self
+
+    def create(self):
+        """
+        Create the DSS instance
+
+        :return: Created DSS Instance
+        :rtype: :class:`dataikuapi.fm.instances.FMAWSInstance`
+        """
+        instance = self.client._perform_tenant_json("POST", "/instances", body=self.data)
+        return FMAWSInstance(self.client, instance)
+
+
+class FMAzureInstanceCreator(FMInstanceCreator):
+    def create(self):
+        """
+        Create the DSS instance
+
+        :return: Created DSS Instance
+        :rtype: :class:`dataikuapi.fm.instances.FMAzureInstance`
+        """
+        instance = self.client._perform_tenant_json("POST", "/instances", body=self.data)
+        return FMAzureInstance(self.client, instance)
 
 
 class FMInstance(object):
     """
     A handle to interact with a DSS instance.
-    Do not create this directly, use :meth:`FMClient.get_instance` or :meth: `FMClient.create_instance`
+    Do not create this directly, use :meth:`FMClient.get_instance` or :meth: `FMClient.new_instance_creator`
     """
     def __init__(self, client, instance_data):
         self.client  = client
@@ -176,19 +203,6 @@ class FMInstance(object):
         self.instance_data['automatedSnapshotRetention'] = keep
         self.save()
 
-    def set_elastic_ip(self, enable, elasticip_allocation_id):
-        """
-        Set a public elastic ip for this instance
-
-        :param boolan enable: Enable the elastic ip allocation
-        :param str elaticip_allocation_id: The AWS ElasticIP allocation ID or the Azure Public IP ID
-        """
-        self.instance_data['awsAssignElasticIP'] = enable
-        self.instance_data['awsElasticIPAllocationId'] = elasticip_allocation_id
-        self.instance_data['azureAssignElasticIP'] = enable
-        self.instance_data['azurePublicIPId'] = elasticip_allocation_id
-        self.save()
-
     def set_custom_certificate(self, pem_data):
         """
         Set the custom certificate for this instance
@@ -198,6 +212,32 @@ class FMInstance(object):
         param: str pem_data: The SSL certificate
         """
         self.instance_data['sslCertificatePEM'] = pem_data
+        self.save()
+
+
+class FMAWSInstance(FMInstance):
+    def set_elastic_ip(self, enable, elasticip_allocation_id):
+        """
+        Set a public elastic ip for this instance
+
+        :param boolan enable: Enable the elastic ip allocation
+        :param str elaticip_allocation_id: AWS ElasticIP allocation ID
+        """
+        self.instance_data['awsAssignElasticIP'] = enable
+        self.instance_data['awsElasticIPAllocationId'] = elasticip_allocation_id
+        self.save()
+
+
+class FMAzureInstance(FMInstance):
+    def set_elastic_ip(self, enable, public_ip_id):
+        """
+        Set a public elastic ip for this instance
+
+        :param boolan enable: Enable the elastic ip allocation
+        :param str public_ip_id: Azure Public IP ID
+        """
+        self.instance_data['azureAssignElasticIP'] = enable
+        self.instance_data['azurePublicIPId'] = public_ip_id
         self.save()
 
 
