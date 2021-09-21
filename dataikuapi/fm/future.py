6a5ee567548a7af12facb0a8d93173b6887fa62e
Thibaud Baas <thibaud.baas@dataiku.com>
2021-09-21 18:21:25 +0200

    FM: format

--- dataikuapi/fm/future.py
+++ dataikuapi/fm/future.py
@@ -1,29 +1,35 @@
 import sys, time
 
+
 class FMFuture(object):
     """
     A future on the DSS instance
     """
-    def __init__(self, client, job_id, state=None, result_wrapper=lambda result: result):
-       self.client = client
-       self.job_id = job_id
-       self.state = state
-       self.state_is_peek = True
-       self.result_wrapper = result_wrapper
+
+    def __init__(
+        self, client, job_id, state=None, result_wrapper=lambda result: result
+    ):
+        self.client = client
+        self.job_id = job_id
+        self.state = state
+        self.state_is_peek = True
+        self.result_wrapper = result_wrapper
 
     @staticmethod
-    def from_resp(client, resp,result_wrapper=lambda result: result):
+    def from_resp(client, resp, result_wrapper=lambda result: result):
         """Creates a DSSFuture from a parsed JSON response"""
-        return FMFuture(client, resp.get('jobId', None), state=resp, result_wrapper=result_wrapper)
+        return FMFuture(
+            client, resp.get("jobId", None), state=resp, result_wrapper=result_wrapper
+        )
 
     @classmethod
     def get_result_wait_if_needed(cls, client, ret):
-        if 'jobId' in ret:
+        if "jobId" in ret:
             future = FMFuture(client, ret["jobId"], ret)
             future.wait_for_result()
             return future.get_result()
         else:
-            return ret['result']
+            return ret["result"]
 
     def abort(self):
         """
@@ -36,7 +42,8 @@ class FMFuture(object):
         Get the status of the future, and its result if it's ready
         """
         self.state = self.client._perform_tenant_json(
-            "GET", "/futures/%s" % self.job_id, params={'peek' : False})
+            "GET", "/futures/%s" % self.job_id, params={"peek": False}
+        )
         self.state_is_peek = False
         return self.state
 
@@ -45,7 +52,8 @@ class FMFuture(object):
         Get the status of the future, and its result if it's ready
         """
         self.state = self.client._perform_tenant_json(
-            "GET", "/futures/%s" % self.job_id, params={'peek' : True})
+            "GET", "/futures/%s" % self.job_id, params={"peek": True}
+        )
         self.state_is_peek = True
         return self.state
 
@@ -53,10 +61,14 @@ class FMFuture(object):
         """
         Get the future result if it's ready, raises an Exception otherwise
         """
-        if self.state is None or not self.state.get('hasResult', False) or self.state_is_peek:
+        if (
+            self.state is None
+            or not self.state.get("hasResult", False)
+            or self.state_is_peek
+        ):
             self.get_state()
-        if self.state.get('hasResult', False):
-            return self.result_wrapper(self.state.get('result', None))
+        if self.state.get("hasResult", False):
+            return self.result_wrapper(self.state.get("result", None))
         else:
             raise Exception("Result not ready")
 
@@ -64,22 +76,26 @@ class FMFuture(object):
         """
         Checks whether the future has a result ready
         """
-        if self.state is None or not self.state.get('hasResult', False):
+        if self.state is None or not self.state.get("hasResult", False):
             self.get_state()
-        return self.state.get('hasResult', False)
+        return self.state.get("hasResult", False)
 
     def wait_for_result(self):
         """
         Wait and get the future result
         """
-        if self.state.get('hasResult', False):
-            return self.result_wrapper(self.state.get('result', None))
-        if self.state is None or not self.state.get('hasResult', False) or self.state_is_peek:
+        if self.state.get("hasResult", False):
+            return self.result_wrapper(self.state.get("result", None))
+        if (
+            self.state is None
+            or not self.state.get("hasResult", False)
+            or self.state_is_peek
+        ):
             self.get_state()
-        while not self.state.get('hasResult', False):
+        while not self.state.get("hasResult", False):
             time.sleep(5)
             self.get_state()
-        if self.state.get('hasResult', False):
-            return self.result_wrapper(self.state.get('result', None))
+        if self.state.get("hasResult", False):
+            return self.result_wrapper(self.state.get("result", None))
         else:
             raise Exception("No result")
