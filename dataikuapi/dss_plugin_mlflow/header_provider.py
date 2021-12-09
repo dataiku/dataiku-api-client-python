67cf94b30b4c917c9d4103e05143fa3013c9cc3a
Valentin Thorey <valentin.thorey@dataiku.com>
2021-12-09 09:08:16 +0100

    Add DSS plugin for MLflow (#191)
    * Add DSS plugin for MLflow
    
    * Add doc to mlflow plugin
    
    * Fix inconsistent wording
    
    * Prevent creating multiple tempdir for mlflow plugin
    
    * Set host while setting up mlflow plugin
    
    * Rename MLflow plugin for consistency
    
    * Add artifact part of the mlflow plugin
    
    * Add log_artifact to mlflow plugin
    
    * Add log_artifacts and list_artifacts
    
    * Adapt wording to backend
    
    * Add deleta and download to artifact plugin
    
    * Add context manager for dss plugin
    
    * Revert formatting
    
    * Fix typo
    
    Co-authored-by: Ludovic Pénet <ludovic.penet@dataiku.com>
    
    Co-authored-by: Ludovic Pénet <ludovic.penet@dataiku.com>

---
+++ dataikuapi/dss_plugin_mlflow/header_provider.py
@@ -0,0 +1,15 @@
+import os
+
+class PluginDSSHeaderProvider:
+
+    def in_context(self):
+        env_variables = {"DSS_MLFLOW_HEADER", "DSS_MLFLOW_TOKEN", "DSS_MLFLOW_PROJECTKEY"}
+        if len(env_variables.difference(set(os.environ))) == 0:
+            return True
+
+    def request_headers(self):
+        headers = {
+            os.environ.get("DSS_MLFLOW_HEADER"): os.environ.get("DSS_MLFLOW_TOKEN"),
+            "x-dku-mlflow-project-key": os.environ.get("DSS_MLFLOW_PROJECTKEY"),
+        }
+        return headers
---
+++ dataikuapi/dss_plugin_mlflow/utils.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import tempfile
+
+
+def load_dss_mlflow_plugin():
+    """ Function to dynamically add entrypoints for MLflow
+
+    MLflow uses entrypoints==0.3 to load entrypoints from plugins at import time.
+    This function adds dss-mlflow-plugin entrypoints dynamically by adding them in sys.path
+    at call time.
+    """
+    tempdir = tempfile.mkdtemp()
+    plugin_dir = os.path.join(tempdir, "dss-plugin-mlflow.egg-info")
+    if not os.path.isdir(plugin_dir):
+        os.makedirs(plugin_dir)
+    with open(os.path.join(plugin_dir, "entry_points.txt"), "w") as f:
+        f.write(
+            "[mlflow.request_header_provider]\n"
+            "unused=dataikuapi.dss_plugin_mlflow.header_provider:PluginDSSHeaderProvider\n"
+            "[mlflow.artifact_repository]\n"
+            "dss-managed-folder=dataikuapi.dss_plugin_mlflow.artifact_repository:PluginDSSManagedFolderArtifactRepository\n"
+        )
+    # Load plugin
+    sys.path.insert(0, tempdir)
+    return tempdir
