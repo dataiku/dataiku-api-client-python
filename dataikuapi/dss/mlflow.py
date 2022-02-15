24fe5bd74a874dc7adf4d28653937820c0f45777
Ludovic Pénet <ludovic.penet@dataiku.com>
2022-02-15 16:30:41 +0100

    Add list_experiments to MLflow extensions. Document a bit list_models.

--- dataikuapi/dss/mlflow.py
+++ dataikuapi/dss/mlflow.py
@@ -11,8 +11,27 @@ class DSSMLflowExtension(object):
         self.project_key = project_key
 
     def list_models(self, run_id):
+        """
+        Returns the list of models of given run
+
+        :param run_id: run_id for which to return a list of models
+        :type run_id: str
+        """
         response = self.client._perform_http(
             "GET", "/api/2.0/mlflow/extension/models/{}".format(run_id),
+            headers={"x-dku-mlflow-project-key": self.project_key}
+        )
+        return response.json()
+
+    def list_experiments(self):
+        """
+        Returns the list of experiments in the DSS project for which MLflow integration
+        is setup
+
+        :rtype: dict
+        """
+        response = self.client._perform_http(
+            "GET", "/api/2.0/mlflow/experiments/list",
             headers={"x-dku-mlflow-project-key": self.project_key}
         )
         return response.json()
